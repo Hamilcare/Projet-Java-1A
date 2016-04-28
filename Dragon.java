2da9cba155d7c531b9d108f11955e3377a3c6381
Traucken <xtraucken@gmail.com>
2016-04-28 16:42:32 +0200

    fix Dragon
    Ajout des déplacements + refonte de la toString()

--- Dragon.java
+++ Dragon.java
@@ -4,10 +4,35 @@ public class Dragon extends Bushi implements Deplacable {
 		super(abs, ord, 3, 0);
 	}
 
+	public ArrayList<Bushi> listerDeplacement(Plateau p) {
+
+		int i;
+		int j;
+		ArrayList<Bushi> possible = new ArrayList<Bushi>();
+
+		// i & j repr�sente la valeur du d�calage par rapport aux coordonn�es
+		// courantes
+		for (i = -1; i <= 1; i++) {
+
+			for (j = 1; j >= -1; j--) {
+				// System.out.println("x= " + (this.abs + i) + " y =" +
+				// (this.ord + j));
+				if(p.plateau[this.ord +j][this.abs + i].etat != 0 ){
+					if (this.reachable(this.abs + 2 * i, this.ord + 2 * j, p)) {
+						possible.add(p.plateau[this.ord + 2 * j][this.abs + 2 * i]);
+					}
+				}
+			}
+
+		}
+		return possible;
+
+	}
+	
 	@Override
 	public String toString() {
 
-		return "d " + this.abs + " " + this.ord;
+		return "Dragon [" + this.abs + ", " + this.ord+"]";
 
 	}
 }
