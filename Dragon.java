49d7cab4a17902d056f38360188a841ca61deccc
Traucken <xtraucken@gmail.com>
2016-04-29 14:42:57 +0200

    update(2) Dragon FTW + javadoc

--- Dragon.java
+++ Dragon.java
@@ -6,7 +6,7 @@ public class Dragon extends Bushi implements Deplacable {
 		super(abs, ord, 3, 0);
 	}
 
-	/**
+/**
 	 * listerDeplacement permet de d'ajouter les deplacements possibles d'un bushi a une arraylist
 	 * @param p le plateau de jeu 
 	 * @return possible Renvoie une ArrayList contenant les deplacements possibles de l'instance courante de Dragon
@@ -25,10 +25,11 @@ public class Dragon extends Bushi implements Deplacable {
 			for (j = 1; j >= -1; j--) {
 				// System.out.println("x= " + (this.abs + i) + " y =" +
 				// (this.ord + j));
-				if(p.plateau[this.ord +j][this.abs + i].etat != 0 && p.plateau[this.ord +j][this.abs + i].etat > 0){
-					if (this.reachable(this.abs + 2 * i, this.ord + 2 * j, p)) {
+				if(this.reachable(this.abs + i, this.ord +j, p) && p.plateau[this.ord +j][this.abs + i].etat != 0 
+						&& p.plateau[this.ord +j][this.abs + i].etat > 0 
+						&& this.reachable(this.abs + 2 * i, this.ord + 2 * j, p)){
 						possible.add(p.plateau[this.ord + 2 * j][this.abs + 2 * i]);
-					}
+					
 				}
 			}
 
@@ -36,7 +37,7 @@ public class Dragon extends Bushi implements Deplacable {
 		return possible;
 
 	}
-
+	
 	@Override
 	public String toString() {
 
